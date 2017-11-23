e3fbf5a189314b79d0e3185f22a6ac11f11692e1
ASUSUser <carausu.catrinel@gmail.com>
2017-11-23 22:15:04 +0200

    Modify Classes to display card components based on an object list

--- WebApp/ClientApp/ilift/src/components/ScheduleCard.js
+++ WebApp/ClientApp/ilift/src/components/ScheduleCard.js
@@ -4,7 +4,7 @@ export class ScheduleCard extends Component {
 
     render() {
 
-        const imageUrl = this.props.imageUrl;
+        const image = this.props.image;
         const courseName = this.props.name;
         const instructorName = this.props.instructorName;
         const time = this.props.time;
@@ -12,7 +12,7 @@ export class ScheduleCard extends Component {
         return (
             <div className="col-md-3 col-sm-6">
                 <div className="program program-schedule">
-                    <img src={require({imageUrl})} alt="Cycling"/>
+                    <img src={image} alt="Cycling"/>
                     <small>{time}</small>
                     <h3>{courseName}</h3>
                     <span>{instructorName}</span>
