3057c91abf6424a3f345858562288ece9079ff05
sabina1296 <alexaelenasabina@gmail.com>
2017-12-12 01:11:14 +0200

    implemented delete and filter on trainers list admin

--- WebApp/ClientApp/ilift/src/components/TrainersList.js
+++ WebApp/ClientApp/ilift/src/components/TrainersList.js
@@ -1,80 +1,194 @@
-import React, { Component, PropTypes } from 'react';
-import VirtualList from 'react-virtual-list';
+import React, { Component } from 'react';
+export class TrainersList extends React.Component {
 
+  constructor(props) {
+    super(props);
 
+    this.state = {};
+    this.state.filterText = "";
+    const trainers=[
+    {id:1,name: 'Mihai Alexandru Badila',category:'Body Trainer'},
+    {id:2,name:'Catrinel Carausu',category:'TRX,Swimming Instructor'},
+    {id:3,name:'Denisa Bica',category:'Boxing trainer'},
+    {id:4,name:'Codrin Strambei',category:'HIIT Instructor'},
+    {id:5,name:'Sabina Alexa',category:'Zumba Instructor'},
+    {id:6,name:'Bocioc Titus',category:'Crossfit Instructor'},
+    {id:7,name:'Nicu Bodea',category:'Bodybuilding Trainer'},
+    {id:8,name:'Cristian Baciu',category:'Yoga Instructor'}
+    ]
+    this.state.trainers = trainers;
 
+  }
+  handleUserInput(filterText) {
+    this.setState({filterText: filterText});
+  };
+  handleRowDel(trainer) {
+    var index = this.state.trainers.indexOf(trainer);
+    this.state.trainers.splice(index, 1);
+    this.setState(this.state.trainers);
+  };
 
-const TrainersList = (MyList,itemsList) => {
-  let MyVirtualList = VirtualList()(MyList);
+  handleAddEvent(evt) {
+    var id = (+ new Date() + Math.floor(Math.random() * 999999)).toString(36);
+    var t = {
+      id: id,
+      name: "",
+      category: "",
+    }
+    var items=[];
+    items[0]=t;
+    for(var i=1;i<=this.state.trainers.length;i++)
+    {
+        items[i]=this.state.trainers[i-1];
+    }
+    const state={
+      trainers:items,
+    }
+    this.setState(state);
 
-  return class MyConfigurableList extends Component {
-    constructor() {
-      super();
+  }
 
-      const defaultItemCount = 3;
-
-      const items = itemsList;
-      
-      const state = {
-        itemHeight: 85,
-        itemCount: defaultItemCount,
-        items: items,
-        contained: true,
-        containerHeight: 400,
-        itemBuffer: 0,
-      };
-      
-      this.state = state;
+  handleTrainerTable(evt) {
+    var item = {
+      id: evt.target.id,
+      name: evt.target.name,
+      value: evt.target.value
     };
+  var trainers = this.state.trainers.slice();
+  var newTrainers = trainers.map(function(trainer) {
 
-    addTrainer = () => {
-      const items = [];
-      items[0]={id:99,name:this.refs.newTrainerName.value , position:this.refs.newTrainerPosition.value};
-      for (var i = 1; i <= this.state.items.length ; i++) {
-        items[i] = this.state.items[i-1];
+    for (var key in trainer) {
+      if (key == item.name && trainer.id == item.id) {
+        trainer[key] = item.value;
+
       }
-      
-      const state = {
-        items: items,
-      };
-      this.setState(state);
+    }
+    return trainer;
+  });
+    this.setState({trainers:newTrainers});
+  };
+  render() {
+
+    return (
+      <div>
+        <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)}/>
+        <TrainerTable onTrainerTableUpdate={this.handleTrainerTable.bind(this)} onRowAdd={this.handleAddEvent.bind(this)} onRowDel={this.handleRowDel.bind(this)} trainers={this.state.trainers} filterText={this.state.filterText}/>
+      </div>
+    );
+
+  }
+
+}
+class SearchBar extends React.Component {
+  handleChange() {
+    this.props.onUserInput(this.refs.filterTextInput.value);
+  }
+  render() {
+    return (
+      <div>
+
+        <input type="text" placeholder="Search..." value={this.props.filterText} ref="filterTextInput" onChange={this.handleChange.bind(this)}/>
+
+      </div>
+
+    );
+  }
+
+}
+
+class TrainerTable extends React.Component {
+
+  render() {
+    var onTrainerTableUpdate = this.props.onTrainerTableUpdate;
+    var rowDel = this.props.onRowDel;
+    var filterText = this.props.filterText;
+    var bodystyle = {
+      height: 250,
+       overflow: 'scroll',
+       display:'block'
     };
 
-    render() {
-      return (
-        <div>
-          <div className="row">
+    var trainer = this.props.trainers.map(function(trainer) {
+      if (trainer.name.indexOf(filterText) === -1) {
+        return;
+      }
+      return (<TrainerRow onTrainerTableUpdate={onTrainerTableUpdate} trainer={trainer} onDelEvent={rowDel.bind(this)} key={trainer.id}/>)
+    });
+    return (
+      <div className="row">
+      <div className="row"></div>
+        <div className="col-xs-6" id="container" ref="container" >
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                </tr>
+              </thead>
 
-            <div className="col-xs-6" id="container" ref="container" style={this.state.contained ? { overflow: 'scroll', height: this.state.containerHeight } : {}}>
-
-              <MyVirtualList
-                items={this.state.items}
-                itemBuffer={this.state.itemBuffer}
-                itemHeight={this.state.itemHeight}/>
-                </div>
-              <div className="col-xs-1">
-              </div>
-            
-
-            <div className="col-xs-5" id="container" >
-            <div className="form-group">
-              <h3>Add a new trainer </h3>
-            </div>
-            <div className="form-group">
-              <input  className="form-control" type="text" placeholder="Trainer Name" id="newTrainerName" ref="newTrainerName"  />
-            </div>
-            <div className="form-group">
-              <input  className="form-control" type="text" placeholder="Trainer Description" id="newTrainerPosition" ref="newTrainerPosition" />  
-            </div>
-            <div className="form-group">
-              <input type="button" name="add_trainer" value="Add Trainer" onClick={this.addTrainer} />
-            </div>
+              <tbody style={bodystyle}>
+                {trainer}
+              </tbody>
+            </table>
+        </div>
+        <div className="col-xs-1">
+        </div>
+        <div className="col-xs-5" id="container" >
+          <div className="form-group">
+            <h3>Add a new trainer </h3>
           </div>
+          <div className="form-group">
+            <input  className="form-control" type="text" placeholder="Trainer Name" id="newTrainerName" ref="newTrainerName"  />
+          </div>
+          <div className="form-group">
+            <input  className="form-control" type="text" placeholder="Trainer Description" id="newTrainerPosition" ref="newTrainerPosition" />  
+          </div>
+          <div className="form-group">
+            <button type="button" onClick={this.props.onRowAdd} className="btn btn-success center">Add</button>
           </div>
         </div>
-      );
-    };
-  };
-};
+  </div>
+    );
 
-export default TrainersList;
+  }
+
+}
+
+class TrainerRow extends React.Component {
+  onDelEvent() {
+    this.props.onDelEvent(this.props.trainer);
+
+  }
+  render() {
+
+    return (
+      <tr className="eachRow">
+        <EditableCell onTrainerTableUpdate={this.props.onTrainerTableUpdate} cellData={{
+          "type": "name",
+          value: this.props.trainer.name,
+          id: this.props.trainer.id
+        }}/>
+        <EditableCell onTrainerTableUpdate={this.props.onTrainerTableUpdate} cellData={{
+          type: "category",
+          value: this.props.trainer.category,
+          id: this.props.trainer.id
+        }}/>
+        <td className="del-cell">
+          <input type="button" onClick={this.onDelEvent.bind(this)} value="X" className="del-btn"/>
+        </td>
+      </tr>
+    );
+
+  }
+
+}
+class EditableCell extends React.Component {
+
+  render() {
+    return (
+      <td>
+        <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onTrainerTableUpdate}/>
+      </td>
+    );
+
+  }
+
+}
--- WebApp/ClientApp/ilift/src/pages/TrainersPageAdmin.js
+++ WebApp/ClientApp/ilift/src/pages/TrainersPageAdmin.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {HeaderAdmin} from '../components/HeaderAdmin';
 import {FooterAdmin} from '../components/FooterAdmin';
-import TrainersList from '../components/TrainersList';
+import {TrainersList} from '../components/TrainersList';
 
 
 
@@ -16,29 +16,7 @@ const trainers=[
 {id:8,name:'Cristian Baciu',position:'Yoga Instructor'}
 ]
 
-const MyList = ({
-  virtual,
-  itemHeight,
-}) => (
-  <ul className="media-list list-group" style={virtual.style}>
-    {virtual.items.map((item) => (
-      <li key={`item${item.id}`} className="list-group-item" style={{height: itemHeight }}>
-        <div className="media-left">
-          <img className="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2ODc1IiB5PSIzMiIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjY0eDY0PC90ZXh0PjwvZz48L3N2Zz4=" />
-        </div>
 
-        <div className="media-body">
-          <h4 className="media-heading">{item.name} : <h5>{item.position}</h5></h4>
-        </div>
-         <div className="media-right">
-        <button>Delete</button>
-        </div>
-      </li>
-    ))}
-  </ul>
-);
-
-const MyConfigurableList = TrainersList(MyList,trainers);
 
 
 export class TrainersPageAdmin extends Component {
@@ -70,7 +48,7 @@ export class TrainersPageAdmin extends Component {
         <div className="container">
           <div className="row about">
             <div className="col-md-12 col-md-offset-0 animate-box">
-              <MyConfigurableList/>
+              <TrainersList/>
             </div>
 
           </div>
