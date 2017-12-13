18044a4819a85302a909348a8c98daab0465ff88
Nicolae BODEA <nbodea@bitdefender.com>
2017-12-13 23:18:35 +0200

    get all courses and get all trainers no longer needed a logged in user to respond

--- WebApp/ServerApp/interface/get_all_trainers.php
+++ WebApp/ServerApp/interface/get_all_trainers.php
@@ -25,32 +25,9 @@ else{
     $token_ok = true;
     $data = null;
 
-    try
-    {
-        $data = $tokenService->validateToken($token);
-        $user = $ctrl->uctrl->get_user_by_name($data['name']);
-        if($user["user_type"] != $data["role"])
-        {
-            throw new Exception("Mismatch user role");
-        }
-    }
-    catch(Exception $e)
-    {
-        $token_ok = false;
-    }
-
-    if($token_ok == false)
-    {
-        $message->answer = "Error";
-        $message->reason = "Invalid token given";
-        echo json_encode($message);
-    }
-    else
-    {
-        $message->answer = "Success";
-        $message->trainers = $ctrl->tctrl->getAllTrainers();
-        echo json_encode($message);
-    }
+    $message->answer = "Success";
+    $message->trainers = $ctrl->tctrl->getAllTrainers();
+    echo json_encode($message);
 
 }
 
